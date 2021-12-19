7d4e92dda95da44e21400dffe8cb8769a810e8be
karisDev <karjaakirill@gmail.com>
2021-12-19 14:24:01 +0300

    поддержка mui для nextjs

--- epic-api/frontend/pages/login.tsx
+++ epic-api/frontend/pages/login.tsx
@@ -1,11 +1,24 @@
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react'
+
+const useStyles = makeStyles({
+    loginContainer: {
+        backgroundColor: '#121212',
+        widht: '100%',
+        height: '100vh',
+    }
+})
 
 interface LoginProps {
 
 }
 
 const Login: React.FC<LoginProps> = ({}) => {
-    return (<div>Login</div>);
+    const classes = useStyles();
+    return (<Container className={classes.loginContainer}>
+        <div>Asd</div>
+    </Container>);
 }
 
 export default Login
